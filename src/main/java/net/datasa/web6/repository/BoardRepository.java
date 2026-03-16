f1ea30426c7152085d08c5e89d022063ca3a0570
KSJ <aalice198912@gmail.com>
2026-03-16 09:38:59 +0900

    260314 0938 project start

--- src/main/java/net/datasa/web6/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package net.datasa.web6.repository;
-
-import net.datasa.web6.domain.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
-}
--- src/main/java/net/datasa/web6/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package net.datasa.web6.repository;
-
-import net.datasa.web6.domain.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-}
